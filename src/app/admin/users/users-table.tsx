f872ae25de675f52a0db8c5464c8924acdf36b2f
Georgii Smirnov <93800248+exovisor@users.noreply.github.com>
2024-01-28 12:51:43 +0300

    Add default query options

--- src/app/admin/users/users-table.tsx
+++ src/app/admin/users/users-table.tsx
@@ -8,7 +8,12 @@ import type {queryOptionsSchema} from "@/server/schema/query";
 import type {z} from "zod";
 
 export function UsersTable() {
-	const [queryOptions, setQueryOptions] = useState<z.infer<typeof queryOptionsSchema>>({})
+	const [queryOptions, setQueryOptions] = useState<z.infer<typeof queryOptionsSchema>>({
+		paginationOptions: {
+			pageIndex: 0,
+			pageSize: 10
+		}
+	})
 	const { data } = api.user.getUsers.useQuery(queryOptions);
 	return <DataTable columns={columns} setQueryOptions={setQueryOptions} payload={data} />
 }
